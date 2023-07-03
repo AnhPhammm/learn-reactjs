79db8fa191fab6b2c508dda7175daa4b8a966097
AnhPhammm <tuap240199@gmail.com>
2023-07-03 22:52:37 +0700

    add_routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import logo from "./logo.svg";
 import "./App.css";
 import TodoFeature from "./features/Todo";
 import AlbumFeature from "./features/Album";
+import { Route, Link, NavLink } from "react-router-dom";
+import { Redirect, Switch } from "react-router-dom/cjs/react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature/>
-      <AlbumFeature />
+      Header
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "16px 0",
+          gap: "8px",
+        }}
+      >
+        <NavLink to="/todos">Todo</NavLink>
+        <NavLink to="/albums" activeClassName="active-menu">
+          Albums
+        </NavLink>
+      </div>
+      <Switch>
+        <Redirect from="/home" to="/" exact/>
+        <Redirect from="/post-list/:postId" to="/post/:postId" exact/>
+
+        <Route path="/" component={TodoFeature} exact/>
+        <Route path="/todos" component={TodoFeature} />
+        <Route path="/albums" component={AlbumFeature} />
+      </Switch>
+      Footer
     </div>
   );
 }
